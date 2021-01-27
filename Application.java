7c9aab5ee0c02b0f349373ca3f508a733d86abb6
Michael <michaelminorsb@gmail.com>
2021-01-26 20:53:45 -0800

    First commit

---
+++ Application.java
@@ -0,0 +1,13 @@
+package com.weekzero;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("please print");
+		
+		
+	}
+
+}
